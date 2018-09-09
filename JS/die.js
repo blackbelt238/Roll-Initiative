4d15a0ebd0a363caed97fc7d25b2dd57b34dbc64
Ethan Peterson <cap2010.ep@gmail.com>
2018-09-09 00:34:40 -0600

    Remove 'var's from declarations

--- JS/die.js
+++ JS/die.js
@@ -3,8 +3,8 @@ function rollDie(sides) {
 }
 
 function rollDice(number, sides) {
-  var sum = 0;
-  for (var i = 0; i < number; i++) {
+  sum = 0;
+  for (i = 0; i < number; i++) {
     sum += rollDie(sides);
   }
   return sum;
