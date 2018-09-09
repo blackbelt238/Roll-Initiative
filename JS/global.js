e2447b7a45b32147249fba5a15df895a9ff7d869
Ethan Peterson <cap2010.ep@gmail.com>
2018-09-09 00:35:54 -0600

    Add the ability to roll initiative for all characters in the table.

--- JS/global.js
+++ JS/global.js
@@ -1,31 +1,31 @@
-var table = []
+table = []
+pageTableBody = document.getElementById("initiative").children[1];
 
-var c1 = new Character("Adran", 4);
+c1 = new Character("Adran", 4);
 table.push(c1);
 
-var c2 = new Character("Jacob", 3);
+c2 = new Character("Jacob", 3);
 table.push(c2);
 
-var c3 = new Character("Galanthus Titarius", -1);
+c3 = new Character("Galanthus Titarius", -1);
 table.push(c3);
 
 // given a Character object, add it to table#initiative
 function addCharacterToTable(character) {
-  pageTableBody = document.getElementById("initiative").children[1];
-  var newRow = document.createElement('tr');
+  newRow = document.createElement('tr');
 
   // create the initiative column
-  var initCol = document.createElement('td');
-  initCol.appendChild(document.createTextNode(0)); // start the character's initiative at 0
+  initCol = document.createElement('td');
+  initCol.appendChild(document.createTextNode(character.initiative)); // start the character's initiative at 0
   newRow.appendChild(initCol);
 
   // create the name column
-  var nameCol = document.createElement('td');
+  nameCol = document.createElement('td');
   nameCol.appendChild(document.createTextNode(character.name));
   newRow.appendChild(nameCol);
 
   // create the dexterity modifier column
-  var dexModCol = document.createElement('td');
+  dexModCol = document.createElement('td');
   dexModCol.appendChild(document.createTextNode(character.dexMod));
   newRow.appendChild(dexModCol);
 
@@ -33,8 +33,26 @@ function addCharacterToTable(character) {
   pageTableBody.appendChild(newRow);
 }
 
+// adds all characters to table#initiative as rows
 function populateTable() {
-  for (var i = 0; i < table.length; i++) {
-    addCharacterToTable(table[i]);
+  for (key in table) {
+    character = table[key];
+    addCharacterToTable(character);
   }
+}
+
+// rolls initiative for all characters in table#initiative
+function rollForCharacters() {
+  // wipe all rows from the table
+  while (pageTableBody.firstChild) {
+    pageTableBody.removeChild(pageTableBody.firstChild);
+  }
+
+  // roll initiative for each character in the table
+  for (key in table) {
+    character = table[key];
+    character.rollInitiative();
+  }
+
+  populateTable();
 }
